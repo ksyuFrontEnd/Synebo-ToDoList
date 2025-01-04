3465ad355561fc1c9418fa6951ba2efade3c6e19
Oksana <ksyubaranik@gmail.com>
2025-01-04 13:24:30 +0100

    Delete emptyList item if there are tasks to do in the list

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const form = document.querySelector('#form');
 const taskInput = document.querySelector('#taskInput');
 const todoList = document.querySelector('#todoList');
+const emptyList = document.querySelector('#emptyList');
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
@@ -22,6 +23,11 @@ form.addEventListener('submit', function(e) {
     // Clear input after adding the task and return focus
     taskInput.value = "";
     taskInput.focus();
+
+    // Delete emptyList item if there are tasks to do in the list
+    if(todoList.children.length > 1) {
+        emptyList.classList.add('none')
+    }
                     
 })
 
