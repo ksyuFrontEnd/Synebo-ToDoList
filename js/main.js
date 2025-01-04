b2628dedb0093e9b981d2346cd2cd1132e243802
Oksana <ksyubaranik@gmail.com>
2025-01-04 13:56:39 +0100

    Made functionality to delete task from the list

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const taskInput = document.querySelector('#taskInput');
 const todoList = document.querySelector('#todoList');
 const emptyList = document.querySelector('#emptyList');
 
+// Add task
 form.addEventListener('submit', addTask);
 
 function addTask(e) {
@@ -30,6 +31,24 @@ function addTask(e) {
     if(todoList.children.length > 1) {
         emptyList.classList.add('none');
     }
+}
+
+// Delete task
+todoList.addEventListener('click', deleteTask);
+
+function deleteTask(event) {
+
+    // Delete task from the list
+    if (event.target.dataset.action === 'delete') {
+        const parentNode = event.target.closest('li'); 
+        parentNode.remove();
+    }
+
+    // Show emptyList item if there are no tasks in the list
+    if (todoList.children.length === 1) {
+        emptyList.classList.remove('none');
+    }
+    
 }
 
 
