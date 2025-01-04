8e2f0969f6de987808a2b82c7e51f69ff63b1a1b
Oksana <ksyubaranik@gmail.com>
2025-01-04 12:47:50 +0100

    Made functionality to add tasks to the list

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-console.log('Hi');
+const form = document.querySelector('#form');
+const taskInput = document.querySelector('#taskInput');
+const todoList = document.querySelector('#todoList');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    // Add new task to the list
+    const taskText = taskInput.value;
+    
+    const taskHTML = `
+                    <li class="list-group-item">
+                        <span class="task-title">${taskText}</span>
+                        <div class="task-item__buttons">
+                            <button type="button" data-action="done" class="btn-action">Done</button>
+                            <button type="button" data-action="delete" class="btn-action">X</button>
+                        </div>
+                    </li>`
+
+    todoList.insertAdjacentHTML('beforeend', taskHTML);
+                    
+})
+
